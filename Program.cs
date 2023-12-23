d2b487df9be21d7ce57e644bf9cf860ddd6bf8a1
anascode23 <anas.ali@ecutbildning.se>
2023-12-23 01:21:32 +0000

    project complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter your birthyear to enter: ");
+var userinput = Console.ReadLine();
+if (int.TryParse(userinput, out var year))
+{
+    var birthdate = new DateTime(year, 1, 1);
+    var currentyear = DateTime.Now.Year;
+    var age = currentyear - birthdate.Year;
+    if (age < 20)
+        Console.WriteLine("Sorry, you are too young to enter");
+
+
+    else
+        Console.WriteLine("Wellcome in and Enjoy!");
+}
